fc65a965f684f20087b395a6c4cbbd42ea903028
lucdv <lucdv>
2022-03-16 03:51:25 +0700

    fix deploy

--- TLPDFGenerator/Program.cs
+++ TLPDFGenerator/Program.cs
@@ -18,12 +18,8 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthorization();
